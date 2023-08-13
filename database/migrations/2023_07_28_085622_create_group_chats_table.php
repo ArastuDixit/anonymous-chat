bde9bf00560e6038dc889242fffd2d02f835e1e6
unknown <arastu.dixit13@gmail.com>
2023-08-14 00:46:19 +0530

    Initial commit

---
+++ database/migrations/2023_07_28_085622_create_group_chats_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('browser_id')->nullable();
+            $table->mediumText('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_chats');
+    }
+};
